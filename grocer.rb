810a518271673c1ed8ad5872cbc305342026578d
Enrique Pittaluga <ep7m@me.com>
2020-03-10 03:08:05 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,6 +2,21 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+
+  i = 0 
+  
+  while ( i < collection.length) do
+    
+    if collection[i][:item] == name
+      return collection[i]
+    end
+    
+    i += 1
+    
+  end
+  
+  return nil
+  
 end
 
 def consolidate_cart(cart)
@@ -9,18 +24,75 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  
+  new_cart = []
+  lookup_hash = {}
+  
+  i = 0 
+  
+  while (i < cart.length) do
+    
+    name = cart[i][:item]
+    
+    if (!lookup_hash[name])
+      lookup_hash[name] = 1
+      new_cart << cart[i][:item]
+      new_cart[:count] = 1
+      
+    else 
+      j = 0
+       while (j < new_cart.length) do
+         if new_cart[j][:item] == name
+           new_cart[:count] += 1
+         j += 1
+       end
+      
+    end
+    
+    i += 1
+  end
+  
+  
+  return new_cart
+  
 end
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  
+  
+  
+  
+  
 end
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  
+  updated_cart = []
+  
+  i = 0 
+  while (i < cart.length) do
+    
+    if (!cart[i][:clearance])
+      updated_cart << cart[i]
+    else
+      price = cart[i][:clearance] * 0.2
+      price = cart[i][:clearance] - price
+      price.round(2)
+      cart[i][:clearance] = price
+      updated_cart << cart[i]
+    end
+    
+    i += 1
+  end
+  
+  return updated_cart
+  
 end
 
 def checkout(cart, coupons)
