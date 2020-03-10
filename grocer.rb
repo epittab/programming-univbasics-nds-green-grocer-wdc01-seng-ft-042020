eda51d106a0878382d4a4bb74e7ea9618a1ada44
Enrique Pittaluga <ep7m@me.com>
2020-03-10 04:09:35 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -70,8 +70,6 @@ def apply_coupons(cart, coupons)
       
       if (cart[i][:item] == coupons[j][:item] && cart[i][:count] >= coupons[j][:num])
         
-#        updated_price = coupons[j][:price]/coupons[j][:num]
-        
         coupon_item = {
           :item => "#{cart[i][:item]} W/COUPON",
           :price => coupons[j][:cost]/coupons[j][:num],
